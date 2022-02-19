a41e578f35d32e93d895aabbc2a181a9cdb76bec
everardo277 <rene.contrerasari@nube.unadmexico.mx>
2022-02-19 16:35:15 -0600

    actualizacion 1

--- js/02-01-TiposDatosJavaScript-UJS.js
+++ js/02-01-TiposDatosJavaScript-UJS.js
@@ -18,3 +18,13 @@ var objecto = {
     telefono : "55443322"
 }
 console.log(objecto);
+
+let hola= "saludos a todos";
+console.log(hola);
+
+let name;
+console.log(name);
+
+let nulo = null;
+
+console.log(nulo);
